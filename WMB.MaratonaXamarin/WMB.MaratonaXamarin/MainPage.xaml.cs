8b404d94791ed3f28deb3f008517713d320ed0bb
Leonardo Dorathoto <leonardod@outlook.com>
2017-06-09 00:00:15 -0300

    .

--- WMB.MaratonaXamarin/WMB.MaratonaXamarin/MainPage.xaml.cs
+++ WMB.MaratonaXamarin/WMB.MaratonaXamarin/MainPage.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WMB.MaratonaXamarin.ViewModels;
+﻿using WMB.MaratonaXamarin.ViewModels;
 using Xamarin.Forms;
 
 namespace WMB.MaratonaXamarin
